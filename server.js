da2dc3c68ebefd49d5f36570f48a949de2f8276b
NuReach <nureach46@gmail.com>
2024-04-26 10:35:27 +0700

    connect to mongo

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 import express from 'express';
 import data from './data.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+  console.log('Connect To DB');
+})
+.catch((err)=>{
+  console.log(err.message);
+})
 
 const app = express();
 const PORT = 3000;
