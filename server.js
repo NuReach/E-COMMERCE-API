3afc46f91db30fdbac1d5c06aad4cd85c334f2b8
NuReach <nureach46@gmail.com>
2024-04-28 22:06:20 +0700

    update order detail

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.use(`/api/users`,userRoutes);
 
 app.use(`/api/orders`,orderRouter);
 
+app.get(`/api/key/paypal` , (req,res)=>{
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+})
+
 
 // Start the server
 app.listen(PORT, () => {
