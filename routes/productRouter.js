ee0d727d3703cefb2132dfe568710f094b492dae
NuReach <nureach46@gmail.com>
2024-04-29 23:15:00 +0700

    pagination

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Product from '../models/productModel.js';
+import expressAsyncHandler from 'express-async-handler';
 
 const productRoute = express.Router();
 
@@ -79,5 +80,53 @@ productRoute.get('/', async (req, res) => {
       res.status(500).send('Error deleting product');
     }
   });
+
+  productRoute.get('/get/categories', expressAsyncHandler (async (req, res) => {
+    try {
+      const categories = await Product.find().distinct('category');
+      res.send(categories);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send({message : 'Error fetching categories' });
+    }
+  }));
+
+  productRoute.get('/search/result', expressAsyncHandler (async (req, res) => {
+    const query = req.query.query || 'all';
+    const category = req.query.category || 'all';
+    const min = req.query.min || 'all';
+    const max = req.query.max ||  'all';
+    const rating = req.query.rating || 'all';
+    const order = req.query.order || 'newest';
+    const page = req.query.page || 0;
+    const limit = 6;
+    try {
+        const searchQuery = {};
+        if (query !== 'all') {
+          searchQuery.$or = [
+            { name: { $regex: query, $options: 'i' } },
+            { description: { $regex: query, $options: 'i' } }
+          ];
+        }
+        if (category !== 'all') {
+          searchQuery.category = category;
+        }
+        if (min !== 'all' && max !== 'all') {
+          searchQuery.price = { $gte: parseFloat(min), $lte: parseFloat(max) };
+        }
+        if (rating !== 'all') {
+          searchQuery.rating = { $lte: parseFloat(rating) };
+          }
+        const count = await Product.countDocuments(searchQuery);
+        const products = await Product.find(searchQuery)
+        .sort(order !== 'newest' ? { createdAt: -1 } : {}) // Sort by order if provided
+        .skip((page - 1) * limit)
+        .limit(limit);
+      res.send({ products, page, count , pages : Math.ceil(count/limit) });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send({message : 'Error fetching categories' });
+    }
+  }));
 
   export default productRoute;
