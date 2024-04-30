6e0315cdab56f113c4e8c664686a308834d27bed
NuReach <nureach46@gmail.com>
2024-05-01 01:52:23 +0700

    update user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ import expressAsyncHandler from 'express-async-handler';
 import { generateToken } from '../utils/generateToken.js';
 import bcrypt from 'bcryptjs';  
 import { isAuth } from '../utils/isAuth.js';
+import { isAdmin } from '../utils/IsAdmin.js';
 
 
 const userRoutes = express.Router();
@@ -19,6 +20,51 @@ userRoutes.get(
         res.status(403).send({ message : 'Not authorized as admin'});
       }
     });
+  
+
+  userRoutes.get(
+    '/allUsers/list',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+      const users = await User.find({});
+      res.send(users);
+    })
+  );
+
+  userRoutes.get(
+    '/:id',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+      const user = await User.findById(req.params.id);
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({ message: 'User Not Found' });
+      }
+    })
+  );
+
+  userRoutes.put(
+    '/:id',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+      const user = await User.findById(req.params.id);
+      if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+        user.password = bcrypt.hashSync(req.body.password, 8);
+        const updatedUser = await user.save();
+        res.send({ message: 'User Updated', user: updatedUser });
+      } else {
+        res.status(404).send({ message: 'User Not Found' });
+      }
+    })
+  );
+  
 
   userRoutes.get('/:id', async (req, res) => {
     try {
